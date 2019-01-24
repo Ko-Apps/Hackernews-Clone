aa35de5b61f26ed3716f21d4c951eda801dcd9f1
Voltis <v.agolli@live.com>
2019-01-24 22:29:51 +0100

    fix prisma endpoint

--- server/src/generated/prisma-client/index.js
+++ server/src/generated/prisma-client/index.js
@@ -12,6 +12,6 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `https://eu1.prisma.sh/vagolli/hackernews-clone/hackernews-clone`
+  endpoint: `https://eu1.prisma.sh/vltsagolli/hackernews/prisma`
 });
 exports.prisma = new exports.Prisma();
